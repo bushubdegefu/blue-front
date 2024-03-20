6c8750bd9dccd6a4231a66eea0c07792d5d539ca
bushubeke <beimdegefu@gmail.com>
2024-03-20 13:10:01 +0300

    login message added 1.0.2

--- src/store/client.js
+++ src/store/client.js
@@ -1,6 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 // const axios = require('axios').default;
 // 10.22.129.62
 // https://goblue-back.onrender.com
-export const blueClient = axios.create({ baseURL: 'http://localhost:3500/api/v1',
-                                        timeout: 10000,})
+export const blueClient = axios.create({
+  baseURL: "https://goblue-back.onrender.com/api/v1",
+  timeout: 10000,
+});
